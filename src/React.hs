74a9cd4bb815dfdd9257700b2ad6585c73f4650d
Ryan Trinkle <ryan@trinkle.org>
2023-07-04 11:57:07 -0400

    Export React.Export from React

--- src/React.hs
+++ src/React.hs
@@ -1,8 +1,9 @@
 module React (module X) where
 
 import React.Component as X
+import React.Element as X
+import React.Export as X
+import React.Hook as X
 import React.JSaddle as X
 import React.Misc as X
 import React.Types as X
-import React.Hook as X
-import React.Element as X
